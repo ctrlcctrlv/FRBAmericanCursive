adfc861918beed272d4487e03b05c2a51b0d87f4
Fredrick Brennan <copypaste@kittens.ph>
2021-12-27 22:50:13 -0500

    [patterned] Add dashed fonts

--- mk/patterned.mk
+++ mk/patterned.mk
@@ -1,16 +1,26 @@
 BUILD_DATA := build_data/dotted.tsv
+WIDTH_MINIMUM := 50.0
 
-# Build the patterned fonts...
 .PHONY .ONESHELL: patterned-dotted
 patterned-dotted:
-	# Build dotted
+	DASHLEN=0.00000001 PREPENDNAME=Dotted make patterned-dotted-dashed
+
+.PHONY .ONESHELL: patterned-dashed
+patterned-dashed:
+	DASHLEN=20 PREPENDNAME=Dashed make patterned-dotted-dashed
+
+# Build the patterned fonts...
+.PHONY .ONESHELL: patterned-dotted-dashed
+patterned-dotted-dashed:
+	# Build dotted or dashed
 	cat $(BUILD_DATA) | parallel --bar --jobs 7 --colsep '\t' '
 		GLYPHS=`find $(FONTFAMILY)-SOURCE.ufo/glyphs/ -type f -not -name contents.plist -not -name space.glif` 
 		ASTERISK='*'
-		DASHDESC="0.00000001 "`perl -e "print {2}$${ASTERISK}1.5"`
-		#DASHDESC="5 "`perl -e "print {2}$${ASTERISK}1.5"`
+		WIDTHADJ=`perl -e "\\$$calc = {2}$${ASTERISK}1.5; \\$$calc = (\\$$calc >= $(WIDTH_MINIMUM) ? \\$$calc : $(WIDTH_MINIMUM)); print \\"\\$$calc\\";"`
+		CULLAREA=`perl -e "print ({2} >= $(WIDTH_MINIMUM) ? 0.3 : 0.7);"`
+		DASHDESC="$(DASHLEN) $$WIDTHADJ"
 		#DASHDESC="{2} {2}"
-		STYLENAME=Dotted{1} WIDTH={2} OS2WEIGHT={3} DASHDESC="$$DASHDESC" GLYPHS="$$GLYPHS" CULLAREA=0.30 make patterned-dotted-template
+		STYLENAME=$(PREPENDNAME){1} WIDTH={2} CULLWIDTH=$$WIDTHADJ OS2WEIGHT={3} DASHDESC="$$DASHDESC" GLYPHS="$$GLYPHS" CULLAREA=$$CULLAREA make patterned-dotted-template
 	'
 
 .PHONY .ONESHELL: patterned-dotted-debug
@@ -19,15 +29,15 @@ patterned-dotted-debug:
 
 .PHONY .ONESHELL: patterned-dotted-template
 patterned-dotted-template:
-	rm -rf build/$(FONTFAMILY)-$(STYLENAME).ufo
-	cp -r build/$(FONTFAMILY)-`./scripts/os2weight_to_namedweight.py $(OS2WEIGHT)`.ufo build/$(FONTFAMILY)-$(STYLENAME).ufo
-	./scripts/fudge_fontinfo.py build/$(FONTFAMILY)-$(STYLENAME).ufo $(STYLENAME) $(OS2WEIGHT)
+	UFO="build/$(FONTFAMILY)-$(STYLENAME).ufo"
+	rm -rf "$$UFO"
+	cp -r build/$(FONTFAMILY)-`./scripts/os2weight_to_namedweight.py $(OS2WEIGHT)`.ufo "$$UFO"
+	./scripts/fudge_fontinfo.py "$$UFO" "$(FONTFAMILY)" "$(FONTFAMILY_H)" $(STYLENAME) $(OS2WEIGHT)
 	CULLAREA=`perl -e 'use Math::Trig; print pi() * (($(WIDTH) / 2.0) ** 2.0) * $(CULLAREA)'`
-	CULLWIDTH=`perl -e 'print $(WIDTH) * 0.65'`
-	parallel --bar "MFEKstroke DASH -o build/$(FONTFAMILY)-$(STYLENAME).ufo/glyphs/{/} -i $(FONTFAMILY)-SOURCE.ufo/glyphs/{/} -d $(DASHDESC) -w $(WIDTH) -c -W $$CULLWIDTH -a $$CULLAREA -l" <<< "$$GLYPHS"
+	CULLWIDTHADJ=`perl -e 'print ($(WIDTH) * 0.65) + ($(WIDTH) >= $(WIDTH_MINIMUM) ? 0 : $(CULLWIDTH))'`
+	parallel --bar "MFEKstroke DASH -o $$UFO/glyphs/{/} -i $(FONTFAMILY)-SOURCE.ufo/glyphs/{/} -d $(DASHDESC) -w $(WIDTH) -c -W $$CULLWIDTHADJ -a $$CULLAREA -l" <<< "$$GLYPHS"
 	ARGS=`./scripts/fontmake_args.sh`
-	$(PYTHON) -m fontmake --keep-overlaps --verbose DEBUG -u build/$(FONTFAMILY)-$(STYLENAME).ufo --output-path dist/$(FONTFAMILY)-$(OS2WEIGHT)-$(STYLENAME).otf -o otf $$ARGS && printf '\033[1;31m Generated $(FONTFAMILY)-$(STYLENAME).ufo\033[0m\n'
-	>&2 echo 'Built $(FONTFAMILY)-$(STYLENAME) w/ dash desc == `$(DASHDESC)`'
+	$(PYTHON) -m fontmake --keep-overlaps --verbose DEBUG -u "$$UFO" --output-path dist/$(FONTFAMILY)-$(OS2WEIGHT)-$(STYLENAME).otf -o otf $$ARGS && printf '\033[1;31m Generated '"$$UFO"'\033[0m w/ dash desc == `$(DASHDESC)`\n'
 
 .PHONY .ONESHELL: patterned-apb
 patterned-apb:
@@ -41,13 +51,14 @@ patterned-ap:
 
 .PHONY .ONESHELL: patterned-template
 patterned-template:
-	rm -rf build/$(FONTFAMILY)-$(STYLENAME).ufo
-	cp -r build/$(FONTFAMILY)-`./scripts/os2weight_to_namedweight.py $(OS2WEIGHT)`.ufo build/$(FONTFAMILY)-$(STYLENAME).ufo
-	./scripts/fudge_fontinfo.py build/$(FONTFAMILY)-$(STYLENAME).ufo $(STYLENAME) $(OS2WEIGHT)
+	UFO="build/$(FONTFAMILY)-$(STYLENAME).ufo"
+	rm -rf "$$UFO"
+	cp -r build/$(FONTFAMILY)-`./scripts/os2weight_to_namedweight.py $(OS2WEIGHT)`.ufo "$$UFO"
+	./scripts/fudge_fontinfo.py "$$UFO" "$(FONTFAMILY)" "$(FONTFAMILY_H)" $(STYLENAME) $(OS2WEIGHT)
 	parallel --bar 'MFEKstroke PAP --out build/$(FONTFAMILY)-$(STYLENAME).ufo/glyphs/{/} --path $(FONTFAMILY)-SOURCE.ufo/glyphs/{/} --pattern $(PATTERN) -m repeated --sx $(MFEKSTROKE_SCALE) --sy $(MFEKSTROKE_SCALE) -s 0 --spacing 15 --overdraw 15%% -Q' <<< "$$GLYPHS"
 	ARGS=`./scripts/fontmake_args.sh`
 	$(PYTHON) -m fontmake --keep-overlaps --verbose DEBUG -u build/$(FONTFAMILY)-$(STYLENAME).ufo --output-path dist/$(FONTFAMILY)-$(OS2WEIGHT)-$(STYLENAME).otf -o otf $$ARGS && printf '\033[1;31m Generated $(FONTFAMILY)-$(STYLENAME).ufo\033[0m\n'
 
 .PHONY: patterned
 patterned:
-	make -j3 patterned-dotted patterned-apb patterned-ap
+	make -j4 patterned-dotted patterned-dashed # patterned-apb patterned-ap
