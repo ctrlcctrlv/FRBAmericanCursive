c380feac47fd4bf2ce19069cd08a06b1d45cfee4
Fredrick Brennan <copypaste@kittens.ph>
2022-01-18 03:55:45 -0500

    tsv_to_mark.py → generate mkmk too when appropriate

--- scripts/tsv_to_mark.py
+++ scripts/tsv_to_mark.py
@@ -16,16 +16,20 @@ fea.statements.append(Comment("# Warning: Auto-generated file. See scripts/tsv_t
 p = Parser(f, followIncludes=True, includeDir='.')
 ff = p.parse()
 glyphclasses = [c for c in ff.statements if isinstance(c, GlyphClassDefinition)]
+glyphclasses_d = {c.name: c.glyphSet() for c in ff.statements if isinstance(c, GlyphClassDefinition)}
 marks = [c for c in glyphclasses if c.name == "GDEFMarks"]
 assert len(marks) > 0, "No marks in GDEF.fea?"
 all_marks = marks[0].glyphSet()
+stroke_marks = [c for c in glyphclasses if c.name == "stroke_marks"]
+assert len(marks) > 0, "No stroke_marks in classes.fea?"
+all_stroke_marks = stroke_marks[0].glyphSet()
 tails = [c for c in glyphclasses if c.name == "tails"]
 if len(tails) > 0:
     all_tails = tails[0].glyphSet()
 
 
 _, csv_fn = sys.argv
-no_filter = "NOFILTER" in os.environ and os.environ["NOFILTER"].strip() == "1"
+#no_filter = "NOFILTER" in os.environ and os.environ["NOFILTER"].strip() == "1"
 
 handled_classes = dict()
 
@@ -35,7 +39,30 @@ with open(os.path.abspath(os.path.join(os.path.dirname(__file__), *("..", "build
     r = csv.reader(numbers_tsvf, delimiter="\t")
     for i, row in enumerate(r): NUMBERS_OFFSETS["stroke{}".format(i)] = (row[0], row[1])
 
-rules = list()
+rules = dict()
+rules_glyphs = dict()
+
+all_mark_classes_in_tsv = dict()
+
+maxlen = dict()
+
+int_to_mc = lambda i: "stroke{}".format(i)
+mc_to_int = lambda mc: int("".join(filter(str.isdigit, mc)))
+prev_mc = lambda mc: "stroke{}".format(mc_to_int(mc)-1)
+next_mc = lambda mc: "stroke{}".format(mc_to_int(mc)+1)
+
+with open(csv_fn) as csvf:
+    r = csv.reader(csvf, delimiter="\t")
+    for row in r:
+        if len(row) < 4: continue
+        mc = row[3]
+        glyph = row[0]
+        if mc not in all_mark_classes_in_tsv:
+            all_mark_classes_in_tsv[mc] = list()
+        all_mark_classes_in_tsv[mc].append(glyph)
+        if "stroke" in mc:
+            if glyph not in maxlen or mc_to_int(mc) > maxlen[glyph]:
+                maxlen[glyph] = mc_to_int(mc)
 
 with open(csv_fn) as csvf:
     r = csv.reader(csvf, delimiter="\t")
@@ -54,9 +81,6 @@ with open(csv_fn) as csvf:
 
         (x, y) = (int(x), int(y))
 
-        if glyph == "_notdef":
-            glyph = ".notdef"
-
         if mark_class in NUMBERS_OFFSETS:
             (offset_x, offset_y) = [int(i) for i in NUMBERS_OFFSETS[mark_class]]
         else:
@@ -68,26 +92,42 @@ with open(csv_fn) as csvf:
             handled_classes[mark_class] = mc
             fea.statements.append(mc)
         if glyph in set(set(all_marks) - set(all_tails)):
-            pos = MarkMarkPosStatement(GlyphName(glyph), [(Anchor(x, y), MarkClass(mark_class))])
+            pos = MarkMarkPosStatement(GlyphName(glyph), [(Anchor(x, y), MarkClass(mc)) for mc in list(all_mark_classes_in_tsv.keys())])
+            if not mark_class in rules_glyphs:
+                rules_glyphs[mark_class] = list()
+            rules_glyphs[mark_class].append(glyph)
         elif glyph not in all_tails:
             pos = MarkBasePosStatement(GlyphName(glyph), [(Anchor(x, y), MarkClass(mark_class))])
         else:
             continue
-        rules.append(pos)
+        if not mark_class in rules:
+            rules[mark_class] = list()
+        rules[mark_class].append(pos)
 
 feature = FeatureBlock("mark")
-lookup = LookupBlock("mark_%s" % csv_fn[csv_fn.rindex('/')+1:csv_fn.rindex('.')])
-if not no_filter:
-    markFilteringSet = GlyphClass([g for g in [c.glyphSet() for c in handled_classes.values()] for g in g ])
+featuremkmk = FeatureBlock("mkmk")
+lookup_filename_root = csv_fn[csv_fn.rindex('/')+1:csv_fn.rindex('.')]
+if len(handled_classes) == 1:
+    lookup_names_append = {k: lookup_filename_root for k in handled_classes.keys()}
 else:
-    markFilteringSet = None
-lookup.statements.append(LookupFlagStatement(value=0, markFilteringSet=markFilteringSet))
-lookup.statements += [rule for rule in rules if isinstance(rule, MarkBasePosStatement)]
-lookup_mark = LookupBlock("markmk_%s" % csv_fn[csv_fn.rindex('/')+1:csv_fn.rindex('.')])
-lookup_mark.statements = [rule for rule in rules if isinstance(rule, MarkMarkPosStatement)]
-feature.statements.append(lookup)
-if len(lookup_mark.statements) > 0:
-    feature.statements.append(lookup_mark)
+    lookup_names_append = {k: "{}_{}".format(lookup_filename_root, c.name) for k, c in handled_classes.items()}
+lookups = {k: LookupBlock("mark_"+lookup_names_append[k]) for k, c in handled_classes.items()}
+lookups_mark = {k: LookupBlock("markmk_"+lookup_names_append[k]) for k, c in handled_classes.items()}
+for k, hcv in handled_classes.items():
+    markFilteringSet = GlyphClass(hcv.glyphSet())
+    lookups[k].statements.append(LookupFlagStatement(value=0, markFilteringSet=markFilteringSet))
+    lookups[k].statements += [rule for rule in rules[k] if isinstance(rule, MarkBasePosStatement)]
+    feature.statements.append(lookups[k])
+for k, hcv in handled_classes.items():
+    if k in rules_glyphs:
+        lmgc = list(all_stroke_marks)+rules_glyphs[k]
+    else:
+        lmgc = all_stroke_marks
+    lookups_mark[k].statements.append(LookupFlagStatement(value=0, markFilteringSet=GlyphClass(lmgc)))
+    lookups_mark[k].statements += [rule for rule in rules[k] if isinstance(rule, MarkMarkPosStatement)]
+    if len(lookups_mark[k].statements) > 1:
+        featuremkmk.statements.append(lookups_mark[k])
+fea.statements.append(featuremkmk)
 fea.statements.append(feature)
 
 print(fea.asFea())
