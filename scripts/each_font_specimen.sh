afe03db7c82d4d72b314b1517a80973ef6084aa4
Fred Brennan <copypaste@kittens.ph>
2022-12-19 16:08:03 -0500

    More sane specimen generation thanks to sile-typesetter/sile#1662.

--- scripts/each_font_specimen.sh
+++ scripts/each_font_specimen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-rm -f /tmp/eachfont${FONTFAMILY}-*.pdf specimens/${FONTFAMILY}-specimen.pdf
+rm -f /tmp/silespecimens/eachfont${FONTFAMILY}-*.pdf specimens/${FONTFAMILY}-specimen.pdf
+mkdir -p /tmp/silespecimens || true
 
 echo ${FONTFAMILY_H}
 
@@ -7,8 +8,8 @@ find dist -type f -iname "${FONTFAMILY}*_NOVF.otf" | sort -V | parallel --jobs 3
     export OS2WEIGHT=\`./scripts/os2weight_of_font.py {}\`
     export WEIGHT=\`./scripts/os2weight_to_namedweight.py \$OS2WEIGHT\`
     export STYLE=\`ftdump -n {}|rg '(family:|style:)'|awk 'BEGIN {FS=\"              \"} {print \$2}'|awk 'BEGIN {FS=\"\\n\"; RS=\"\"} {print \$1\$2}'\`
-	(sed \"s/&&FONT&&/{/}/g; s/&&FONTFAMILY&&/${FONTFAMILY}/g; s/&&FONTFAMILY_H&&/${FONTFAMILY_H}/g; s/&&OS2WEIGHT&&/\$OS2WEIGHT/g; s/&&WEIGHT&&/\$WEIGHT/g; s/&&PAGE&&/{#}/g; s/&&STYLE&&/\$STYLE/\" < specimens/eachfont.sil) > /tmp/eachfont{/}.sil &&
-	sile /tmp/eachfont{/}.sil
+	(sed \"s/&&FONT&&/{/}/g; s/&&FONTFAMILY&&/${FONTFAMILY}/g; s/&&FONTFAMILY_H&&/${FONTFAMILY_H}/g; s/&&OS2WEIGHT&&/\$OS2WEIGHT/g; s/&&WEIGHT&&/\$WEIGHT/g; s/&&PAGE&&/{#}/g; s/&&STYLE&&/\$STYLE/\" < specimens/eachfont.sil) > /tmp/silespecimens/eachfont{/}.sil &&
+	sile /tmp/silespecimens/eachfont{/}.sil
 "
 
-pdftk `find /tmp/ -iname "eachfont${FONTFAMILY}-*.pdf"|sort -V` cat output "specimens/${FONTFAMILY_H} specimen.pdf"
+pdftk `find /tmp/silespecimens/ -iname "eachfont${FONTFAMILY}-*.pdf"|sort -V` cat output "specimens/${FONTFAMILY_H} specimen.pdf"
